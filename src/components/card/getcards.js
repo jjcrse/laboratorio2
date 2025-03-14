bcb3b8cc1aa6daa2cdf43af166b3b280e3ede17d
FelipeGd02 <felipe.gonzalez.0225@gmail.com>
2025-03-13 20:17:56 -0500

    primera prueba de cards

---
+++ src/components/card/getcards.js
@@ -0,0 +1,12 @@
+export default function getcards() {
+    return [
+        {
+            title: "Overwatch2",
+            subtitulo: "Nuevas habilidades de personajes y estilos",
+            info: "Se implementa un nuevo sistema de mejoras que permite a los jugadores obtener mejoras menores y mayores durante las partidas, como reducciones en los tiempos de reutilización y modificaciones significativas en habilidades.",
+            author: "Blizzard",
+            fecha: "9 de Marzo de 2025",
+            imagenAutor: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Blizzard_Entertainment_Logo.svg/1200px-Blizzard_Entertainment_Logo.svg.png"
+        },
+    ];
+}
